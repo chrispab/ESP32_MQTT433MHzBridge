5d891bc4e2e30bfeea564b3a49e3f130cac5c2a7
chrispab <cbattisson@gmail.com>
2018-05-17 00:15:13 +0100

    working pre rf24

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,14 @@ void listenForClients(void);
 void LEDBlink(int LEDPin, int repeatNum);
 void callback(char *topic, byte *payload, unsigned int length);
 void reconnectPSClient();
-void reconnectWiFiEsp();
+void reconnectWiFi();
 void operateSocket(uint8_t socketID, uint8_t state);
-void printD(const char *message);
-void printDWithVal(const char *message, int value);
-void printD2Str(const char *str1, const char *str2);
+void printO(const char *message);
+void printOWithVal(const char *message, int value);
+void printO2Str(const char *str1, const char *str2);
+void checkConnections(void);
+void updateTempDisplay(void);
+void printO(int x, int y, char *text);
 
 //OLED display stuff
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE, /* clock=*/22, /* data=*/21); // ESP32 Thing, HW I2C with pin remapping
@@ -47,15 +50,19 @@ bool state = false;
 uint8_t socketNumber = 0;
 
 //misc stuff
-#define LEDPIN 2 //esp32 on board blue LED
+#define LEDPIN 2 //esp32 devkit on board blue LED
 #define CR Serial.println()
-#define SW_VERSION "V0.5"
+#define SW_VERSION "V1.0"
 
 //Global vars
 unsigned long currentMillis = 0;
-unsigned long previousMillis = 0;
-unsigned long intervalMillis = 30000;
-
+//conection check timer
+unsigned long previousConnCheckMillis = 0;
+unsigned long intervalConCheckMillis = 30000;
+//unsigned long currentMillis = 0;
+unsigned long previousTempDisplayMillis = 0;
+unsigned long intervalTempDisplayMillis = 20000;
+char tempStr[17]; // buffer for 16 chars and eos
 
 void setup()
 { //Initialize serial monitor port to PC and wait for port to open:
@@ -65,100 +72,99 @@ void setup()
     u8g2.begin();
     u8g2.clearBuffer();
     u8g2.setFont(u8g2_font_8x13_tf);
-    u8g2.drawStr(30, 10, "MQTT ESP32");
-    u8g2.drawStr(15, 21, "433Mhz Bridge");
-    u8g2.drawStr(45, 32, SW_VERSION);
+    u8g2.drawStr(30, 20, "MQTT ESP32");
+    u8g2.drawStr(15, 32, "433Mhz Bridge");
+    u8g2.drawStr(45, 44, SW_VERSION);
     u8g2.sendBuffer();
-    delay(4000);
+    delay(5000);
 
-    // initialize ESP module
+    //printO("Trying to connect..", 1, 20);
     WiFi.begin();
-    //    printWifiStatus();
     // attempt to connect to Wifi network:
-    while (status != WL_CONNECTED)
-    {
-        Serial.print("Attempting to connect to SSID: ");
-        Serial.println(ssid);
-        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-        status = WiFi.begin(ssid, pass);
-        // wait 5 seconds for connection:
-        delay(10000);
-    }
-    //server.begin();
+    reconnectWiFi();
     // you're connected now, so print out the status:
     printWifiStatus();
     CR;
     reconnectPSClient();
-
     dht.setup(DHTPIN);
 }
 
 void loop()
 {
-    psclient.loop();
+    psclient.loop();     //process any MQTT stuff
+    checkConnections();  // reconnect if reqd
+    updateTempDisplay(); //get and display temp
+}
 
-    //maybe check every n secs to see if client is connected and reconnect if reqd
-
+void checkConnections()
+{
     currentMillis = millis();
-    if (currentMillis - previousMillis > intervalMillis)
+    if (currentMillis - previousConnCheckMillis > intervalConCheckMillis)
     {
-        Serial.println("Checking if WiFi needs reconnect"); // save the last time looped
+        Serial.println("Checking if WiFi or MQTT PSClient needs reconnecting"); // save the last time looped
         if (status != WL_CONNECTED)
         {
-            Serial.println("Wifi Needs reconnect");
-            reconnectWiFiEsp();
+            Serial.println("Wifi Needs reconnecting");
+            reconnectWiFi();
         }
         else
         {
             Serial.println("OK - WiFi still connected");
-            Serial.println(millis());
+            //Serial.println(millis());
         }
 
-        Serial.println("Checking if psclient needs reconnect"); // save the last time looped
+        //Serial.println("Checking if psclient needs reconnect"); // save the last time looped
         if (!psclient.connected())
         {
-            Serial.println("Needs reconnect");
+            Serial.println("PSClient Needs reconnecting");
             reconnectPSClient();
         }
         else
         {
-            Serial.println("OK - ps client still connected");
-            Serial.println(millis());
+            Serial.println("OK - MQTT PS client still connected");
+            //Serial.println(millis());
         }
 
-        previousMillis = currentMillis;
+        previousConnCheckMillis = currentMillis;
+    }
+}
+
+void updateTempDisplay()
+{
+    currentMillis = millis();
+    if (currentMillis - previousTempDisplayMillis > intervalTempDisplayMillis)
+    {
 
         // Read temperature as Celsius (the default)
         float t = dht.getTemperature();
-        char tempStr[9]; //="12345678";buffer
+        //char tempStr[9]; //="12345678";buffer
         // Check if any reads failed and exit early (to try again).
+        char msgStr[]="Message String long enough?";
+        strcpy(msgStr, "Temp: ");
         if (isnan(t))
         {
             Serial.println("Failed to read from DHT sensor!");
-            //return;
-        }
-
-        if (!isnan(t))
-        {
-            dtostrf(t, 5, 2, tempStr);
-            strcat(tempStr,"*C");
-        }
-        else
-        {
             strcpy(tempStr, "-=NaN=-");
         }
+        else // is a number
+        {
+            dtostrf(t, 4, 1, tempStr);
+            strcat(tempStr, "*C");
+        }
 
-        u8g2.clearBuffer();
-        u8g2.setCursor(20, 40);
-        //delay(10);
-        printD2Str("Temp: ", tempStr); 
-        //u8g2.print("*C");
-        delay(10);
-        u8g2.sendBuffer();
+        // u8g2.clearBuffer();
+        // u8g2.setCursor(20, 40);
+        // printO2Str("Temp: ", tempStr);
+        // delay(10);
+        // u8g2.sendBuffer();
+
+        printO(20,40, strcat(msgStr,tempStr));
 
         Serial.print("Temperature: ");
-        Serial.print(t);
-        Serial.println(" *C ");
+        Serial.println(tempStr);
+        //Serial.println(" *C ");
+
+        previousTempDisplayMillis = currentMillis;
     }
 }
 
@@ -182,7 +188,7 @@ void callback(char *topic, byte *payload, unsigned int length)
     CR;
     Serial.print("Message arrived [");
     Serial.print(topic);
-    Serial.print("] ");
+    Serial.print("] : ");
     for (uint8_t i = 0; i < length; i++)
     {
         Serial.print((char)payload[i]);
@@ -213,30 +219,19 @@ void callback(char *topic, byte *payload, unsigned int length)
         newState = 1;
     }
 
-    Serial.println(millis());
-
-    //maybe call a few times
-    Serial.println(F("txing to socket"));
-    //u8g2.clearBuffer();
-
-    Serial.println("txing state to socket");
-
     digitalWrite(LEDPIN, newState);
-    //transmitter.sendUnit(socketNumber, newState); // default 4 tx to socket
     operateSocket(socketNumber, newState);
-    //LEDBlink(LEDPIN, socketNumber); // blink socketNumber times
 }
 
 //0-15, 0,1
 void operateSocket(uint8_t socketID, uint8_t state)
 {
     //this is a blocking routine so need to keep checking messages and
-    //updating vars etc
 
     u8g2.clearBuffer();
     u8g2.setCursor(20, 20);
-    //delay(10);
-    printDWithVal("Socket : ", socketNumber + 1); //physical socket number
+    delay(10);
+    printOWithVal("Socket : ", socketNumber + 1); //physical socket number
 
     u8g2.setCursor(55, 40);
     if (state == 0)
@@ -247,9 +242,6 @@ void operateSocket(uint8_t socketID, uint8_t state)
     {
         u8g2.print("ON");
     }
-
-    //delay(10);
-    //printDWithVal("Socket : ", socketNumber + 1); //physical socket number
     delay(10);
     u8g2.sendBuffer();
 
@@ -260,21 +252,16 @@ void operateSocket(uint8_t socketID, uint8_t state)
         transmitter.sendUnit(socketID, state);
     }
 
-    Serial.println(F("socket state updated"));
-
-    Serial.println(F("OK"));
-    // }
-    // else
-    // {
-    // 	Serial.println(F("not transmitting"));
-    // }
+    Serial.println(F("OK - TX socket state updated"));
 }
 
-void reconnectWiFiEsp()
+void reconnectWiFi()
 {
     // Loop until we're reconnected
     //check is psclient is connected first
     // attempt to connect to Wifi network:
+    printO(1, 20, "Connect WiFi..");
+
     while (status != WL_CONNECTED)
     {
         Serial.print("Attempting to connect to SSID: ");
@@ -293,8 +280,10 @@ void reconnectPSClient()
     //check is psclient is connected first
     while (!psclient.connected())
     {
+        printO(1, 20, "Connect MQTT..");
         Serial.println("Attempting MQTT connection...");
         // Attempt to connect
+        //        if (psclient.connect("ESP32Client","",""))
         if (psclient.connect("ESP32Client"))
         {
             Serial.println("connected to MQTT server");
@@ -343,18 +332,29 @@ void printWifiStatus()
     Serial.print(rssi);
     Serial.println(" dBm");
 }
-void printD(const char *message)
+
+void printO(int x, int y, char *text)
+{
+    u8g2.begin();
+    u8g2.clearBuffer();
+    u8g2.setFont(u8g2_font_8x13_tf);
+    u8g2.drawStr(x, y, text);
+    u8g2.sendBuffer();
+    delay(10);
+}
+
+void printO(const char *message)
 {
     u8g2.print(message);
 }
 
-void printDWithVal(const char *message, int value)
+void printOWithVal(const char *message, int value)
 {
     u8g2.print(message);
     u8g2.print(value);
 }
 
-void printD2Str(const char *str1, const char *str2)
+void printO2Str(const char *str1, const char *str2)
 {
     u8g2.print(str1);
     u8g2.print(str2);
