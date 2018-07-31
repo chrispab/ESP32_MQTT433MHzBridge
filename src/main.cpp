3c94b198e28c65ee24de8f4d012f1beec047a4b6
chrispab <cbattisson@gmail.com>
2018-07-31 08:43:50 +0100

    working email notifications

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ char receive_payload[max_payload_size +
 #define TITLE_LINE1 "ESP32 MQTT"
 #define TITLE_LINE2 "433Mhz Bridge"
 #define TITLE_LINE3 "Wireless Dog"
-#define SW_VERSION "V3.22 Br:\"OO2\""
+#define SW_VERSION "V3.23 Br:\"OO2\""
 
 // Global vars
 unsigned long currentMillis = 0;
