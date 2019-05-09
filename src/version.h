df097c49a86421671c1f5e403816942484ee3cff
chrispab <cbattisson@gmail.com>
2019-05-09 20:17:01 +0100

    improve web page

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
-#define SW_VERSION "V4.35 Br:\"dev\""
+#define SW_VERSION "V4.37 Br:\"dev\""
 #define TITLE_LINE1 "     ESP32"
 #define TITLE_LINE2 "MQTT 433MhZ Bridge"
 #define TITLE_LINE3 "Zone Wireless Dog"
 #define TITLE_LINE4 "MQTT Temp Sensor"
 #define TITLE_LINE5 SW_VERSION
-#define TITLE_LINE6 "PIR"
+#define TITLE_LINE6 "webpage"
 // //#define SYS_FONT u8g2_font_8x13_tf
 // #define SYS_FONT u8g2_font_6x12_tf       // 7 px high
 // #define BIG_TEMP_FONT u8g2_font_fub30_tf //30px hieght
