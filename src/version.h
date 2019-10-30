725630efe8a6ef6e25dcb2e84b2a91a4b222dc30
chrispab <cbattisson@gmail.com>
2019-10-30 18:50:47 +0000

    GMT

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
-#define SW_VERSION "V4.78 Br:\"new NTP\""
+#define SW_VERSION "V4.79 Br:\"new NTP\""
 #define TITLE_LINE1 "     ESP32"
 #define TITLE_LINE2 "MQTT 433MhZ Bridge"
 #define TITLE_LINE3 "Zone RF24 Dog"
 #define TITLE_LINE4 "Temp Light PIR"
 #define TITLE_LINE5 SW_VERSION
-#define TITLE_LINE6 "api test"
+#define TITLE_LINE6 "GMT"
 
 #define CR Serial.println()
