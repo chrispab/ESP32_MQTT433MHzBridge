179529e5d7b405f1a3e34ae40be37f2b8fcbcb18
chrisbattisson@britishtriathlon.org <chrisbattisson@britishtriathlon.org>
2019-12-03 14:49:45 +0000

    4

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
-#define SW_VERSION "V4.80 Br:\"new NTP\""
+#define SW_VERSION "V4.81 Br:\"wip\""
 #define TITLE_LINE1 "     ESP32"
 #define TITLE_LINE2 "MQTT 433MhZ Bridge"
 #define TITLE_LINE3 "Zone RF24 Dog"
 #define TITLE_LINE4 "Temp Light PIR"
 #define TITLE_LINE5 SW_VERSION
-#define TITLE_LINE6 "rest off"
+#define TITLE_LINE6 "argghh!"
 
 #define CR Serial.println()
