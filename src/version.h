214a60f9015a81f4adb3770af1831148bf6a75e7
chrispab <cbattisson@gmail.com>
2020-08-18 00:16:43 +0100

    527 master

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define SW_VERSION "V5.26 Br:\"wip\""
+#define SW_VERSION "V5.27 Br: master"
 #define TITLE_LINE1 "     ESP32"
 #define TITLE_LINE2 "MQTT 433MhZ Bridge"
 #define TITLE_LINE3 "Zone RF24 Dog"
