ca9f24956801e9b89b9466286a58f9f27cc01e09
chrispab <cbattisson@gmail.com>
2020-08-21 23:32:42 +0100

    528

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 #define TITLE_LINE3 "Zone RF24 Dog"
 #define TITLE_LINE4 "Temp Light PIR"
 #define TITLE_LINE5 SW_VERSION
-#define TITLE_LINE6 "fix mqtt time check"
+#define TITLE_LINE6 "pio ota enable"
 
 #define CR Serial.println()
 
