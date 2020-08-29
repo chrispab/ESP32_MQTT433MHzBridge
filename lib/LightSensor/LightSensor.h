c8598fa85d99b1d3cd315e8ad0916938563ebca5
chrispab <cbattisson@gmail.com>
2020-08-29 22:50:02 +0100

    532

--- lib/LightSensor/LightSensor.h
+++ lib/LightSensor/LightSensor.h
@@ -1,39 +1,39 @@
 #ifndef __LIGHTSENSOR_H
 #define __LIGHTSENSOR_H
 
-#include <Arduino.h>
-#include "IOBase.h"
+// #include <Arduino.h>
 #include <config.h>
 
-class LightSensor : public IOBase
-{
-private:
-  //bool state;
-  int pin;
-  bool newLevelFlag;
-  u_int currentLevel;
-  u_int previousLevel = 500;
-  u_int readIntervalMillis = 30000; //min interval between reading sensor in ms
-  u_int lastReadMillis = -30000;
+#include "IOBase.h"
 
-  //hysteresis and state related properties
-  //u_int thresholdLevel = 600;
-  int lowerThresholdLevel = LIGHT_SENSOR_LOWER_THRESHOLD;
-  int upperThresholdLevel = LIGHT_SENSOR_UPPER_THRESHOLD;
-  //bool trigger = false;
+class LightSensor : public IOBase {
+   private:
+    //bool state;
+    int pin;
+    bool newLevelFlag;
+    u_int currentLevel;
+    u_int previousLevel = 500;
+    u_int readIntervalMillis = 30000;  //min interval between reading sensor in ms
+    u_int lastReadMillis = -30000;
 
-  //int currentLevel;
-  //int lowerHys = targetLevel - lowerLevel;
-  //int upperHys = targetLevel + upperLevel;
+    //hysteresis and state related properties
+    //u_int thresholdLevel = 600;
+    int lowerThresholdLevel = LIGHT_SENSOR_LOWER_THRESHOLD;
+    int upperThresholdLevel = LIGHT_SENSOR_UPPER_THRESHOLD;
+    //bool trigger = false;
 
-public:
-  LightSensor(uint8_t pin);
-  u_int getLevel();
-  u_int clearNewLevelFlag();
-  bool hasNewLevel();
-  void setThresholdLevel();
-  int getLightSensor();
-  bool getState();
+    //int currentLevel;
+    //int lowerHys = targetLevel - lowerLevel;
+    //int upperHys = targetLevel + upperLevel;
+
+   public:
+    LightSensor(uint8_t pin);
+    u_int getLevel();
+    u_int clearNewLevelFlag();
+    bool hasNewLevel();
+    void setThresholdLevel();
+    int getLightSensor();
+    bool getState();
 };
 
 #endif
