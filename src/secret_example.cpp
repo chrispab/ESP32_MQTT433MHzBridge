0276bd9a3d7be233cda925e61f80ecf35ef1f3d0
chrispab <cbattisson@gmail.com>
2025-06-14 09:25:22 +0100

    Add secret_example.cpp with configuration constants and API keys

---
+++ src/secret_example.cpp
@@ -0,0 +1,14 @@
+#include "secret.h"
+
+const char* REST_BEARER_TOKEN =
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjZhOWExZTg1MTEwM2JlNDgxYWQ1"
+    "Nzk0ZGMzNGM5NTkzYjk0NDk4YjQ3ZjA1YjBiOTU5ODg2YzM4YzRmMmRmODMyZmE1ODZjZjE1"
+    "NDFmZjBmIn0.";
+
+const char* MY_SSID = "fjgh";
+const char* MY_SSID_PASSWORD = "chgchc";
+const char* APP_PASSWORD = "chcghgc";
+const char* EMAIL_ADDRESS = "ccccchgchgh@gmail.com";
+
+#define IFTTT_API_KEY "chvcvhhch"
+#define IFTTT_EVENT_NAME "cvcvchghgn"
